0ec0bc0b10a323919eddbfd356c626538e968483
Renato <renato145@hotmail.com>
2021-04-28 23:06:17 +0930

    add hostname

--- run_rabbit.sh
+++ run_rabbit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+docker run -it --rm --hostname rabbitmq --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
