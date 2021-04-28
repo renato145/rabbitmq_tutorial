2b8b1219498d878e96b66ce83faeab92ddfc383d
Renato <renato145@hotmail.com>
2021-04-28 23:36:18 +0930

    simplify

--- 01_hello-world/recieve.py
+++ 01_hello-world/recieve.py
@@ -1,19 +1,11 @@
-import pika, sys, os
+import pika
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='hello')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
 
-    def callback(ch, method, properties, body): print(f' [x] Received {body!r}')
-    channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
+def callback(ch, method, properties, body): print(f' [x] Received {body!r}')
+channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
-if __name__ == '__main__':
-    try: main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try: sys.exit(0)
-        except SystemExit: os._exit(0)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
--- 02_work-queues/worker.py
+++ 02_work-queues/worker.py
@@ -1,25 +1,17 @@
-import pika, sys, os, time
+import pika, time
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='task_queue', durable=True)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
 
-    def callback(ch, method, properties, body):
-        print(f' [x] Received {body.decode()!r}')
-        time.sleep(body.count(b'.'))
-        print(' [x] Done')
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+def callback(ch, method, properties, body):
+    print(f' [x] Received {body.decode()!r}')
+    time.sleep(body.count(b'.'))
+    print(' [x] Done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='task_queue', on_message_callback=callback)
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
-if __name__ == '__main__':
-    try: main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try: sys.exit(0)
-        except SystemExit: os._exit(0)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
