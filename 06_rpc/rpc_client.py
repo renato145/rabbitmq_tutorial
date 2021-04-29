3a08eac7785e5b53c746753b00ce7ec084c7ef5a
Renato <renato145@hotmail.com>
2021-04-30 00:00:10 +0930

    tutorial 6 python

---
+++ 06_rpc/rpc_client.py
@@ -0,0 +1,41 @@
+import pika, uuid
+
+routing_key = 'rpc_queue'
+
+class FibonacciRpcClient:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True
+        )
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id: self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=routing_key,
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id
+            ),
+            body=str(n)
+        )
+
+        while self.response is None: self.connection.process_data_events()
+        return int(self.response)
+        
+fibonacci_rpc = FibonacciRpcClient()
+
+n = 30
+print(f' [x] Requesting fib({n})')
+response = fibonacci_rpc.call(n)
+print(f' [.] Got {response}')
